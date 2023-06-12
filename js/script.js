8a260221434f8fbac47592b327450b0eeec62551
marcocareddu <saldumaccu@tuta.io>
2023-06-12 16:15:22 +0200

    JS - Linked DOM elements & Create Var & array

--- js/script.js
+++ js/script.js
@@ -1,1 +1,39 @@
 console.log('JS OK')
+
+// * PHASE 1
+// LINK DOM ELEMENTS
+const rowElement = document.querySelector('row');
+
+// Crate objects array
+const team = [
+    {
+        name: "Wayne Barnett",
+        job: "Founder & CEO",
+        photo: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        name: "Angela Caroll",
+        job: "Chief Editor",
+        photo: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        name: "Walter Gordon",
+        job: "Office Manager",
+        photo: "walter-gordon-office-manager.jpg"
+    },
+    {
+        name: "Angela Lopez",
+        job: "Social Media Manager",
+        photo: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        name: "Scott Estrada",
+        job: "Developer",
+        photo: "scott-estrada-developer.jpg"
+    },
+    {
+        name: "Barbara Ramos",
+        job: "Graphic Designer",
+        photo: "barbara-ramos-graphic-designer.jpg"
+    }
+];
